57d458c466cee6d385566c8d86ea64d66625dd6f
Ethan Rucinski <ethan.rucinski@gmail.com>
2021-03-02 09:00:43 -0600

    updating authentication flows

--- auth/auth.js
+++ auth/auth.js
@@ -1,75 +1,93 @@
 "use strict";
 const express = require("express");
-const callbackUrl = "http://localhost:3000/callback";
-const scope = "https://api.us-east-1.dev.stratoshell.com/stratoshell.taskApi";
-const client_id = "7g72seg6ahnhb4o0hg7ojkd02h";
-const client_secret = "1b490938tnh2k9k5d1g9mp2fcducc42t9a1fmc3h72brcfc6pst8";
-const tokenHost = "https://auth.us-east-1.dev.stratoshell.com";
+const open = require("open");
 const path = require("path");
 
+const callbackUrl = "http://localhost:3000/callback";
+const scope = "https://api.us-east-1.dev.stratoshell.com/stratoshell.taskApi";
+
+const tokenHost = "https://auth.us-east-1.dev.stratoshell.com";
 
 const { AuthorizationCode } = require("simple-oauth2");
-const client = new AuthorizationCode({
-    client: {
-        id: client_id,
-        secret: client_secret,
-    },
-    auth: {
-        tokenHost: tokenHost,
-        tokenPath: "/oauth2/token",
-        authorizePath: "/login",
-    },
-});
 
-// Authorization uri definition
-const authorizationUri = client.authorizeURL({
-    redirect_uri: callbackUrl,
-    scope: scope,
-});
-
-exports.createAuthApp = function (tokenCallback, errorCallback) {
-    let app = express();
-    // Initial page redirecting to Github
-    app.get("/", (_, res) => {
-        console.log(authorizationUri);
-        res.redirect(authorizationUri);
-    });
-
-    // Callback service parsing the authorization token and asking for the access token
-    app.get("/callback", async (req, res) => {
-        const { code } = req.query;
-        const options = {
-            code,
-            scope: scope,
+class Auth {
+    constructor() {
+        this.client_id = "7g72seg6ahnhb4o0hg7ojkd02h";
+        this.client_secret =
+            "1b490938tnh2k9k5d1g9mp2fcducc42t9a1fmc3h72brcfc6pst8";
+        this.client = new AuthorizationCode({
+            client: {
+                id: this.client_id,
+                secret: this.client_secret,
+            },
+            auth: {
+                tokenHost: tokenHost,
+                tokenPath: "/oauth2/token",
+                authorizePath: "/login",
+            },
+        });
+        this.authorizationUri = this.client.authorizeURL({
             redirect_uri: callbackUrl,
-        };
+            scope: scope,
+        });
+        this.accessToken = null;
+    }
 
-        try {
-            const accessToken = await client.getToken(options);
-            //console.log("The resulting token: ", accessToken.token);
-            tokenCallback(accessToken);
-            res.status(200).sendFile(path.join(__dirname + "/success.html"));
-        } catch (error) {
-            console.log(error);
-            console.error("Access Token Error", error.message);
-            errorCallback(error);
-            res.status(500).json("Authentication failed");
-        }
-    });
-    return app;
-};
+    // This creates a new authentication app with a webpage to get a new token with user interraction
+    createAuthApp = async (tokenCallback, errorCallback) => {
+        let app = express();
 
-exports.refresh = function (accessToken) {
-    //if (accessToken.expired()) {
+        app.get("/", (_, res) => {
+            res.redirect(this.authorizationUri);
+        });
+
+        // Callback service parsing the authorization token and asking for the access token
+        app.get("/callback", async (req, res) => {
+            const { code } = req.query;
+            const options = {
+                code,
+                scope: scope,
+                redirect_uri: callbackUrl,
+            };
+
+            console.log(options);
+
+            try {
+                const aT = await this.client.getToken(options);
+                this.accessToken = aT;
+                console.log("Sending response!");
+                await new Promise((resolve) => {
+                    res.status(200).sendFile(
+                        path.join(__dirname + "/success.html"),
+                        (err) => {
+                            if (err) console.log(err);
+                            console.log("sent!");
+                            tokenCallback(aT);
+                            resolve();
+                        }
+                    );
+                });
+            } catch (error) {
+                console.log(error);
+                console.error("Access Token Error", error.message);
+                this.accessToken = null;
+                errorCallback(error);
+                res.status(500).json("Authentication failed");
+            }
+        });
+        return app;
+    };
+
+    refresh = function () {
         const refreshParams = {
             scope: scope,
         };
 
         return new Promise((resolve, reject) => {
-            accessToken
+            this.accessToken
                 .refresh(refreshParams)
                 .then((data) => {
-                    console.log(data)
+                    this.accessToken.token = data.token;
                     resolve(data);
                 })
                 .catch((err) => {
@@ -78,5 +96,37 @@ exports.refresh = function (accessToken) {
                     reject(err);
                 });
         });
-    //}
-};
+    };
+
+    getToken = async function () {
+        if (!this.accessToken) {
+            // Need to get a completely new token all together
+            try {
+                this.accessToken = await new Promise(
+                    async (resolve, reject) => {
+                        let authApp = await this.createAuthApp(resolve, reject);
+                        authApp.listen(3000, async (err) => {
+                            if (err) reject(err);
+                            open("http://localhost:3000");
+                        });
+                    }
+                );
+                return this.accessToken;
+            } catch (err) {
+                console.log("Couldn't get authentication token!");
+                return null;
+            }
+        } else if (this.accessToken.expired()) {
+            try {
+                await this.refresh();
+                return this.accessToken;
+            } catch (err) {
+                return null;
+            }
+        } else {
+            return this.accessToken;
+        }
+    };
+}
+
+exports.Auth = Auth;
