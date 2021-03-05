33a203046b31f6c1ac5414d17cad445afa1d7a01
Ethan Rucinski <ethan.rucinski@gmail.com>
2021-03-04 21:45:39 -0600

    adding clear credentials input for command line tool

--- index.js
+++ index.js
@@ -1,11 +1,55 @@
 #!/usr/bin/env node
 
+const commandLineUsage = require("command-line-usage");
+
 const { Auth } = require("./auth.js");
 const { Task } = require("./task.js");
 const { Connection } = require("./connection.js");
 const auth = new Auth();
 
+const argv = require("minimist")(process.argv.slice(2));
+const sections = [
+    {
+        header: "Stratoshell command line tool",
+        content: "Client CLI utility for using Stratoshell",
+    },
+    {
+        header: "Options",
+        optionList: [
+            {
+                name: "help",
+                description: "Print this help guide",
+                alias: "h",
+                type: Boolean,
+            },
+            {
+                name: "clear-credentials",
+                description: "Delete stored stratoshell credentials",
+                type: Boolean,
+            },
+        ],
+    },
+];
+
+async function handleCLIInputs() {
+    // Check for help option requested
+    if (argv.h) {
+        console.log(commandLineUsage(sections));
+        process.exit(0);
+    }
+
+    // Check for clear credentials requested
+    if (argv["clear-credentials"]) {
+        await auth.deleteSavedToken();
+        console.log("Login credentials cleared!");
+        process.exit(0);
+    }
+}
+
 async function main() {
+    await handleCLIInputs();
+
+    // Otherwise start tool
     console.log("Starting stratoshell command line tool...");
 
     // Create a new task and request remote instance
